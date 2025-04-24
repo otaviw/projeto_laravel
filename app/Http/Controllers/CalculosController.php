8e1f531e1c57707400d0ea2068817aab126742c1
luisfelipecasa <luisfelipecasagrande878@gmail.com>
2025-04-24 11:34:40 -0300

    calculos e keepinho controller

--- app/Http/Controllers/CalculosController.php
+++ app/Http/Controllers/CalculosController.php
@@ -6,7 +6,23 @@ use Illuminate\Http\Request;
 
 class CalculosController extends Controller
 {
-    function soma($x, $y){
-        return 'Soma ' . $x + $y;
+    function somar($x, $y){
+        return 'Soma: ' . $x + $y;
+    }
+
+    function subtrair($x, $y){
+        return 'Subtração: ' . $x - $y;
+    }
+
+    function multiplicar($x, $y){
+        return 'Multiplicação: ' . $x * $y;
+    }
+
+    function dividir($x, $y){
+        return 'Divisão: ' . $x / $y;
+    }
+
+    function quadrado($x){
+        return 'Quadrado: ' . $x * $x;
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\CalculosController;
+use App\Http\Controllers\KeepinhoController;
 use Illuminate\Support\Facades\Route;
 
+
+//testes
 Route::get('/', function () {
     return view('welcome');
 });
@@ -17,4 +21,18 @@ Route::get('/teste/{valor}', function ($valor) {
 Route::get('/soma/{valor1}/{valor2}', function ($valor1, $valor2) {
     $soma = $valor1 + $valor2;
     return "A sua soma é: {$soma}";
+});
+
+//calculos
+Route::prefix('/calc')->group(function () {
+    Route::get('/somar/{x}/{y}', [CalculosController::class,'somar']);
+    Route::get('/subtrair/{x}/{y}', [CalculosController::class,'subtrair']);
+    Route::get('/multiplicar/{x}/{y}', [CalculosController::class,'multiplicar']);
+    Route::get('/dividir/{x}/{y}', [CalculosController::class,'dividir']);
+    Route::get('/quadrado/{x}', [CalculosController::class,'quadrado']);
+});
+
+//Keepinho
+Route::prefix('/keep')->group(function () {
+    Route::get('/', [KeepinhoController::class,'index']);
 });
