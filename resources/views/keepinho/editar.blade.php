00a5341d41dbf00021a172ea20a3e8f64f1bca25
luisfelipecasa <luisfelipecasagrande878@gmail.com>
2025-05-20 08:52:18 -0300

    tradução de erros, validações de formulário

--- resources/views/keepinho/editar.blade.php
+++ resources/views/keepinho/editar.blade.php
@@ -3,13 +3,25 @@
     <p>Seja bem-vindo ao Keepinho, o seu assistente pessoal (melhor do que o google).</p>
 </div>
 
+@if ($errors->any())
+    <div class="error-box">
+        <h3>ERRO!</h3>
+        <ul>
+            @foreach ($errors->all() as $err)
+            <li>{{ $err }}</li>
+            @endforeach
+        </ul>
+    </div>
+    @endif
+
 <form action="{{ route('keep.editarGravar') }}" method="POST">
     @csrf
     @method('PUT')
 
     <input type="hidden" name="id" value="{{ $nota->id }}">
 
-    <textarea placeholder="Escreva sua nota" name="texto" id="" cols="30" rows="10">{{ $nota->texto }}</textarea>
+    <input value="{{ old('titulo', $nota->titulo) }}" type="text" placeholder="Título" name="titulo">
+    <textarea placeholder="Escreva sua nota" name="texto" id="" cols="30" rows="10">{{ old('texto', $nota->texto) }}</textarea>
     <br>
     <input type="submit" value="salvar" style="padding: 5px 10px; font-size: 15px">
 </form>
--- resources/views/keepinho/index.blade.php
+++ resources/views/keepinho/index.blade.php
@@ -1,39 +1,43 @@
 <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 
-<div style="display:flex; flex-direction:column; width:100%; align-items:center;">
+<div class="container">
     <h1>🤯Keepinho</h1>
     <p>Seja bem-vindo ao Keepinho, o seu assistente pessoal (melhor do que o google).</p>
-</div>
 
-<hr style="margin:20px 0px">
-@if ($errors->any())
-<div style="color: red; display:flex; flex-direction: column; align-items:center;">
-    <h3>ERRO!</h3>
-</div>
-@endif
-    <form action="{{ route('keep.gravar') }}" method="POST" style="display:flex; flex-direction: column; align-items:center;">
-    @csrf
-        <input type="text" placeholder="título" name="titulo">
-        <br>
-        <textarea placeholder="Escreva sua nota" name="texto" id="" cols="30" rows="10"></textarea>
-        <br>
-        <input type="submit" value="salvar" style="padding: 5px 10px; font-size: 15px">
+    <hr style="margin:20px 0px">
+
+    @if ($errors->any())
+    <div class="error-box">
+        <h3>ERRO!</h3>
+        <ul>
+            @foreach ($errors->all() as $err)
+            <li>{{ $err }}</li>
+            @endforeach
+        </ul>
+    </div>
+    @endif
+
+    <form action="{{ route('keep.gravar') }}" method="POST">
+        @csrf
+        <input value="{{ old('titulo') }}" type="text" placeholder="Título" name="titulo">
+        <textarea placeholder="Escreva sua nota" name="texto" cols="30" rows="5">{{ old('texto') }}</textarea>
+        <input type="submit" value="Salvar">
     </form>
-<hr style="margin:20px 0px">
 
-<div style="display:flex;">
-    @foreach ($notas as $nota)
-        <div style="background-color:#F9F2C8; border-radius:5px; padding: 10px; font-size: larger; margin: 10px; width: 200px; height: 200px; box-shadow: rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px;">
+    <hr style="margin:20px 0px">
+
+    <div class="notes">
+        @foreach ($notas as $nota)
+        <div class="note-card">
             <h4>{{ $nota->titulo }}</h4>
-            {{ $nota->texto }}
-            <br>
+            <p>{{ $nota->texto }}</p>
             <a href="{{ route('keep.editar', $nota->id) }}">Editar</a>
-            <br>
-            <form action="{{ route('keep.apagar', $nota->id) }}" method="POST">
+            <form class="form-delete" action="{{ route('keep.apagar', $nota->id) }}" method="POST">
                 @method('DELETE')
                 @csrf
                 <input type="submit" value="Apagar">
             </form>
         </div>
-    @endforeach
+        @endforeach
+    </div>
 </div>
