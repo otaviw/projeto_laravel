ff5968da6e2e480d365e08c87b7a1d60a918a811
Otavio Carbone <vivicocarbone@gmail.com>
2025-07-10 11:24:35 -0300

    factory e seed prontas

--- database/migrations/2025_07_03_132303_create_comments_table.php
+++ database/migrations/2025_07_03_132303_create_comments_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
-            $table->string('content');
+            $table->mediumText('content');
 
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->foreignId('post_id')->constrained()->onDelete('cascade');
